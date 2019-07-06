d1bd03f4f033b589aa84dacf27b6dfb29011530a
Olivia Zoe <zoe.i2k1@gmail.com>
2019-07-06 18:34:06 +0200

    Docker-based approach

--- .travis/run.sh
+++ .travis/run.sh
@@ -6,6 +6,21 @@ if [[ "$(uname -s)" == 'Darwin' ]]; then
         eval "$(pyenv init -)"
     fi
     pyenv activate conan
+    export CXX=clang++-$CONAN_APPLE_CLANG_VERSIONS
+    export CC=clang-$CONAN_APPLE_CLANG_VERSIONS
+
+    conan create . LunarWatcher/testing --build missing
+else 
+
+    if [ $CONAN_CLANG_VERSIONS ]
+        export CXX=clang++-$CONAN_CLANG_VERSIONS
+        export CC=clang-$CONAN_CLANG_VERSIONS
+    else 
+        export CXX=g++-$CONAN_GCC_VERSIONS
+        export CXX=gcc-$CONAN_GCC_VERSIONS
+    fi
+
+    docker run -d -p 127.0.0.1:80:4567 -v .:/conan-IXWebSocket $CONAN_DOCKER_IMAGE "cd conan-IXWebSocket; conan create . LunarWatcher/testing --build missing"
 fi
 
-python build.py
+
