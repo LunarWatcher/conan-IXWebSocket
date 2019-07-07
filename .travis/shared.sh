38b9769fc733ee440face24b2359b04801730386
Olivia Zoe <zoe.i2k1@gmail.com>
2019-07-07 19:02:49 +0200

    Attempt to fix response code management

--- .travis/shared.sh
+++ .travis/shared.sh
@@ -4,12 +4,13 @@ echo "Currently on branch $branch"
 if [[ "$branch" == *"release/"* ]]; then
     
     conan create . LunarWatcher/stable --build missing $([ "${IX_OPTIONS}" ] && echo "${IX_OPTIONS}" || echo "")
+
     result = $?
     # If the build succeeded, and we're on a release branch, upload to Bintray!
-    if [ $result == 0  ]; then
+    if [ "$result" == 0  ]; then
         conan remote add origin $CONAN_UPLOAD
         # ... assuming the Bintray API key is defined.
-        if [ $CONAN_PASSWORD ]; then
+        if [ "$CONAN_PASSWORD" ]; then
             conan user -p $CONAN_PASSWORD -r origin $CONAN_REFERENCE
             conan upload -r origin --confirm "IXWebSocket/*"
         fi
