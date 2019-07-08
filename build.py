eed03cc6227c8b81cbe3cebe31d5c913d116dec8
Olivia Zoe <zoe.i2k1@gmail.com>
2019-07-08 14:05:42 +0200

    Remove unsupported build, testing Conan installation of missing packages

--- build.py
+++ build.py
@@ -1,6 +1,16 @@
 from cpt.packager import ConanMultiPackager
+import os 
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager()
+    dockerEntryScript = None
+
+    if "INSTALL_CONAN_PACKAGES" in os.environ:
+        rawPackages = os.environ["INSTALL_CONAN_PACKAGES"].split(",")
+        dockerEntryScript = "conan install "
+        for package in rawPackages:
+            dockerEntryScript += package + " " 
+        dockerEntryScript += "--build missing"
+    builder = ConanMultiPackager(docker_entry_script=dockerEntryScript)
+
     builder.add_common_builds(pure_c=False)
     builder.run()
