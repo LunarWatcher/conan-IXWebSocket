845b211eba520f2a5f105504d83e1153ad959c6c
Olivia Zoe <zoe.i2k1@gmail.com>
2019-11-01 18:50:39 +0100

    Release 6.2.1

--- conanfile.py
+++ conanfile.py
@@ -3,7 +3,7 @@ import os
 
 class IXWebSocketConan(ConanFile):
     name = "IXWebSocket"
-    version = "6.2.0"
+    version = "6.2.1"
     description = "WebSocket client/server"
     topics = ("conan", "IXWebSocket", "socket", "websocket")
     
